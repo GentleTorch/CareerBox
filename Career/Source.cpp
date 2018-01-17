9719d49e13f42a1b460ecb2ea67f308d3b1b07da
Leaf <JesusLeaf@163.com>
2018-01-17 12:27:22 +0800

    demo for bfs and dfs

--- Career/Source.cpp
+++ Career/Source.cpp
@@ -1,28 +1,60 @@
 #include<iostream>
-#include<cstdio>
+#include<string>
+#include<vector>
+#include<queue>
 
 using namespace std;
 
+
+struct node
+{
+	int data;
+	node *left, *right;
+
+	node(int a) :data(a) { left = NULL, right = NULL; }
+};
+
+void BFS(node *root)
+{
+	queue<node*>que;
+	que.push(root);
+	node *tmp;
+	
+	while (!que.empty())
+	{
+		tmp = que.front();
+		cout << tmp->data << " ";
+		if (tmp->left != NULL)
+			que.push(tmp->left);
+		if (tmp->right != NULL)
+			que.push(tmp->right);
+
+		que.pop();
+	}
+
+	cout << endl;
+}
+
+void DFS(node *root)
+{
+	if (root == NULL)return;
+	cout << root->data << " ";
+	DFS(root->left);
+	DFS(root->right);
+}
+
 int main()
 {
-	long long x, k;
-	long long bitNum = 1;
-	long long ans = 0;
-	cin >> x >> k;
+	node *root = new node(1);
+	int i;
+	root->left = new node(3);
+	root->right = new node(5);
+	root->left->right = new node(7);
+	root->right->left = new node(9);
 
-	while (k)
-	{
-		if ((x&bitNum) == 0)
-		{
-			ans += (bitNum*(k & 1));
-			k >>= 1;
-		}
-
-		bitNum <<= 1;
-
-	}
-	
-	cout << ans << endl;
-
+	BFS(root);
+	DFS(root);
 	return 0;
+
 }
+
